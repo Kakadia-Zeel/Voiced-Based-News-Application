40405525d82705df356192a094c881d55ffe9b4f
Kakadiya Zeel <zkzeelkakadiya@gmail.com>
2021-01-04 22:24:05 +0530

    Updates

---
+++ precache-manifest.dbddc72129fda99bde2d393e6ef5ab14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a6fec1a23f44fa654781504f7bc1cf",
+    "url": "/Voiced-Based-News-Application/index.html"
+  },
+  {
+    "revision": "cdf25535ac1c67327d84",
+    "url": "/Voiced-Based-News-Application/static/css/main.60451c08.chunk.css"
+  },
+  {
+    "revision": "ed366fdea3f26c6f067d",
+    "url": "/Voiced-Based-News-Application/static/js/2.faaa0754.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Voiced-Based-News-Application/static/js/2.faaa0754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf25535ac1c67327d84",
+    "url": "/Voiced-Based-News-Application/static/js/main.49dd6b29.chunk.js"
+  },
+  {
+    "revision": "1dffb1124cf60bb21c85",
+    "url": "/Voiced-Based-News-Application/static/js/runtime-main.e5b2adec.js"
+  },
+  {
+    "revision": "c1382b6cbae321245bf675055e618574",
+    "url": "/Voiced-Based-News-Application/static/media/bg4.c1382b6c.jpg"
+  },
+  {
+    "revision": "1fd57fcbca63f57e98c4d5a41c35952a",
+    "url": "/Voiced-Based-News-Application/static/media/logo.1fd57fcb.jpg"
+  }
+]);
